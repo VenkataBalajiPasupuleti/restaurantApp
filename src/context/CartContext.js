ff2e9dd0f080a1ded68fcb7cdae4e574b04caa9a
VenkataBalajiPasupuleti <venkatabalajipasupuleti123@gmail.com>
2025-03-02 08:37:11 +0000

    Restaurant-App

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,34 +1,12 @@
-import {createContext, useContext, useState} from 'react'
+import {createContext} from 'react'
 
-const CartContext = createContext()
+const CartContext = createContext({
+  cartList: [],
+  addCartItem: () => {},
+  removeCartItem: () => {},
+  removeAllCartItems: () => {},
+  incrementCartItemQuantity: () => {},
+  decrementCartItemQuantity: () => {},
+})
 
-export const CartProvider = ({children}) => {
-  const [cart, setCart] = useState({})
-
-  const addToCart = item => {
-    setCart(prev => ({
-      ...prev,
-      [item.dish_id]: (prev[item.dish_id] || 0) + 1,
-    }))
-  }
-
-  const removeFromCart = item => {
-    setCart(prev => {
-      const updatedCart = {...prev}
-
-      if (updatedCart[item.dish_id] > 0) {
-        updatedCart[item.dish_id] -= 1
-      }
-
-      return updatedCart
-    })
-  }
-
-  return (
-    <CartContext.Provider value={{cart, addToCart, removeFromCart}}>
-      {children}
-    </CartContext.Provider>
-  )
-}
-
-export const useCart = () => useContext(CartContext)
+export default CartContext
